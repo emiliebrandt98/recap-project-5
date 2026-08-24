73ac6097bc2563c5ef041fc7896ac06e906d2fd2
7.studio <mail@julianejeske.com>
2026-08-24 14:16:22 +0200

    edit file

---
+++ Components/Spotlight/Spotlight.jsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+
+function getRandomArtPiece(artPieceCard) {
+  const randomIndex = Math.floor(Math.random() * artPieceCard.length);
+  return artPieceCard[randomIndex];
+}
+
+export default function HomePage() {
+   const artPiece = getRandomArtPiece(artPieceCard);
+
+  return (
+    <main>
+     <Image
+        src={artPiece.imageSource}
+        alt={artPiece.name}
+        width={artPiece.dimensions.width}
+        height={artPiece.dimensions.height}
+        style={{ width: "100%", height: "auto" }}
+      />
+      <p>{artPiece.name}</p>
+      <p>{`by ${artPiece.artist}`}</p>
+    </main>
+  );
+}
