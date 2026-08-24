b708af6b0d7d2da88b48fa7f6674173fcba01bb7
7.studio <mail@julianejeske.com>
2026-08-24 11:51:41 +0200

    add Component Spotlight

---
+++ Components/Spotlight/Spotlight.jsx
@@ -0,0 +1,29 @@
+import { ArtPieceCard } from "@/data/ArtPieceCard";
+
+// If your artworks are stored in an Component array such as ArtPieceCard, 
+// you can select a random item like this: 
+// Math.random() * (max - min) + min = 5.95
+// Math.floor(5.95)= 5;
+
+function getRandomArtPiece(ArtPiecesCard) {
+  const randomIndex = Math.floor(Math.random() * ArtPiecesCard.length);
+  return ArtPiecesCard[randomIndex];
+}
+
+
+export default function HomePage() {
+   const artPiece = getRandomArtPiece(ArtPiecesCard);
+
+
+  return (
+    <main>
+      <img
+        src={artPiece.imageSource}
+        alt={artPiece.title}
+      />
+      <p>{artPiece.name}</p>
+      <p>{artPiece.year}</p>
+      <p>{artPiece.genre}</p>
+    </main>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,42 @@
-import { ArtPieceCard } from "@/data/ArtPieceCard";
 
+// <! -- FIRST PART ---> 
+// import { ArtPieceCard } from "@/data/ArtPieceCard";
 
 // If your artworks are stored in an Component array such as ArtPieceCard, 
 // you can select a random item like this: 
 // Math.random() * (max - min) + min = 5.95
 // Math.floor(5.95)= 5;
-function getRandomArtPiece(ArtPiecesCard) {
-  const randomIndex = Math.floor(Math.random() * ArtPiecesCard.length);
-  return ArtPiecesCard[randomIndex];
-}
+
+// function getRandomArtPiece(ArtPiecesCard) {
+//   const randomIndex = Math.floor(Math.random() * ArtPiecesCard.length);
+//   return ArtPiecesCard[randomIndex];
+// }
+
+
+// export default function HomePage() {
+//    const artPiece = getRandomArtPiece(ArtPiecesCard);
+
+
+//   return (
+//     <main>
+//       <img
+//         src={artPiece.imageSource}
+//         alt={artPiece.title}
+//       />
+//       <p>{artPiece.name}</p>
+//       <p>{artPiece.year}</p>
+//       <p>{artPiece.genre}</p>
+//     </main>
+//   );
+// }
+
+
+// <! -- SECOND PART ---> 
+// Create a new component for the Spotlight page.
+
+import Spotlight from "@/components/Spotlight/Spotlight";
 
 
 export default function HomePage() {
-   const artPiece = getRandomArtPiece(ArtPiecesCard);
-
-
-  return (
-    <main>
-      <img
-        src={artPiece.imageSource}
-        alt={artPiece.title}
-      />
-
-      <p>{artPiece.name}</p>
-      <p>{artPiece.year}</p>
-      <p>{artPiece.genre}</p>
-    </main>
-  );
+  return <Spotlight />;
 }
