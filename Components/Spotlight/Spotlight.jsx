001b78361677904ab2e59a102391003d21d3afd8
7.studio <mail@julianejeske.com>
2026-08-24 14:21:49 +0200

    import Component ArtPieceCard

--- Components/Spotlight/Spotlight.jsx
+++ Components/Spotlight/Spotlight.jsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import ArtPieceCard from "Components/ArtPieceCard/ArtPieceCard.jsx"
 
 function getRandomArtPiece(artPieceCard) {
   const randomIndex = Math.floor(Math.random() * artPieceCard.length);
