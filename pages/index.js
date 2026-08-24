aef787ce5c71704281f7f71dc33dda692720d4cf
7.studio <mail@julianejeske.com>
2026-08-24 11:44:10 +0200

    add random art piece function

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,30 @@
+import { ArtPieceCard } from "@/data/ArtPieceCard";
+
+
+// If your artworks are stored in an Component array such as ArtPieceCard, 
+// you can select a random item like this: 
+// Math.random() * (max - min) + min = 5.95
+// Math.floor(5.95)= 5;
+function getRandomArtPiece(ArtPiecesCard) {
+  const randomIndex = Math.floor(Math.random() * ArtPiecesCard.length);
+  return ArtPiecesCard[randomIndex];
+}
+
+
 export default function HomePage() {
+   const artPiece = getRandomArtPiece(ArtPiecesCard);
+
+
   return (
-    <div>
-      <h1>Hello from Next.js</h1>
-    </div>
+    <main>
+      <img
+        src={artPiece.imageSource}
+        alt={artPiece.title}
+      />
+
+      <p>{artPiece.name}</p>
+      <p>{artPiece.year}</p>
+      <p>{artPiece.genre}</p>
+    </main>
   );
 }
