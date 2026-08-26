d1a438ada8118b5d7e1376412ddf490b426388d1
Emilie Brandt <emiliebrandt@gmx.de>
2026-08-26 15:47:06 +0200

    add style to navigation

--- components/Navigation/Navigation.jsx
+++ components/Navigation/Navigation.jsx
@@ -1,21 +1,70 @@
 import Link from "next/link";
+import styled from "styled-components";
 
 export default function Navigation() {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link href="/">Spotlight</Link>
-        </li>
+    <StyledNavigation>
+      <StyledList>
+        <StyledListItem>
+          <StyledLink href="/">Spotlight</StyledLink>
+        </StyledListItem>
 
-        <li>
-          <Link href="/gallery">Gallery</Link>
-        </li>
+        <StyledListItem>
+          <StyledLink href="/gallery">Gallery</StyledLink>
+        </StyledListItem>
 
-        <li>
-          <Link href="/favourites">Favourites</Link>
-        </li>
-      </ul>
-    </nav>
+        <StyledListItem>
+          <StyledLink href="/favourites">Favourites</StyledLink>
+        </StyledListItem>
+      </StyledList>
+    </StyledNavigation>
   );
 }
+
+const StyledNavigation = styled.nav`
+  display: flex;
+  align-items: center;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+
+  height: 4em;
+  background-color: var(--background-color-primary);
+  z-index: 1000;
+`;
+
+const StyledList = styled.ul`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+  width: 100%;
+  height: 100%;
+  list-style: none;
+  padding: 0;
+  margin: 0;
+`;
+
+const StyledListItem = styled.li`
+  flex-grow: 1;
+  height: 100%;
+
+  &:hover {
+    background-color: var(--background-color-secondary);
+  }
+`;
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: var(--text-color-primary);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+
+  &:hover {
+    text-decoration: underline;
+  }
+`;
--- styles.js
+++ styles.js
@@ -11,4 +11,14 @@ export default createGlobalStyle`
     margin: 0;
     font-family: system-ui;
   }
+
+  :root{
+    --background-color-primary: #25373D;
+    --background-color-secondary: #32484F;
+    --text-color-primary: #f2bf16;
+    --text-color-secondary: #E1AD01;
+    --border-primary: #E1AD01;
+    --icon-border: #E1AD01;
+    --icon-fill: #E1AD01;
+  }
 `;
